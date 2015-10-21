0f6cfb32b0971567d14b4202544c75da31b2829d
Jesper Madsen <jesper@sobuno.dk>
2015-10-21 20:22:48 +0200

    ISD reports not using the official format now work

--- swg/model/mail/SWGMailMessage.java
+++ swg/model/mail/SWGMailMessage.java
@@ -417,7 +417,8 @@ public final class SWGMailMessage
      * @return a type
      */
     Type getType() {
-        if (fromLine().endsWith("interplanetary survey droid")) {
+        if (fromLine().endsWith("interplanetary survey droid") || 
+        	fromLine().endsWith("Interplanetary Survey Droid")) {
             long now = System.currentTimeMillis() / 1000;
             now -= (60 * 60 * 20); // max 20 hours old
             return messageDate > now
